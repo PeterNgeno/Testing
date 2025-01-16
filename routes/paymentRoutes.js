71054e2ea538bee38eea60d81cdbcc25f3870e5d
PeterNgeno <peterkipsangngeno@gmail.com>
2025-01-17 00:45:26 +0300

    Delete routes directory

--- routes/paymentRoutes.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const { initiateSTKPush, handleCallback } = require('../controllers/paymentController');
-const router = express.Router();
-
-router.post('/stkpush', initiateSTKPush); // STK Push initiation
-router.post('/callback', handleCallback); // Callback endpoint
-
-module.exports = router;
