2f49e476a11e9da703acaf698fd4da702adfc549
PeterNgeno <peterkipsangngeno@gmail.com>
2025-01-24 22:41:21 +0300

    Update index.js

--- api/index.js
+++ api/index.js
@@ -152,7 +152,7 @@ app.post("/callback", (req, res) => {
 });
 
 // Start the server
-const PORT = process.env.PORT || 2000;
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`Server running on http://localhost:${PORT}`);
 });
